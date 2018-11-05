d804e376237160a0fd059adf0afef4505286b40e
Roman Seleznov <roman.seleznov@gmail.com>
2018-11-05 09:21:49 +0000

    Initial project commit

---
+++ src/main/java/com/creditsuisse/codechallenge/seleznov/roman/logpersister/LogPersisterApplication.java
@@ -0,0 +1,12 @@
+package com.creditsuisse.codechallenge.seleznov.roman.logpersister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPersisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogPersisterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/creditsuisse/codechallenge/seleznov/roman/logpersister/LogPersisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.creditsuisse.codechallenge.seleznov.roman.logpersister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogPersisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
